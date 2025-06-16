cb72e20d5b03ccb1e6c0dfdd4cf57a2929de4cbc
hamistani606 <160989612+hamistani606@users.noreply.github.com>
2025-06-16 15:39:55 -0700

    Update motion_planner.py

--- robots/g1_description/scripts/motion_planner.py
+++ robots/g1_description/scripts/motion_planner.py
@@ -1,4 +1,115 @@
-import sys 
-import moveit_commander
+#!/usr/bin/env python3
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, SRI International
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of SRI International nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Acorn Pooley
+
+## BEGIN_SUB_TUTORIAL imports
+##
+## To use the python interface to move_group, import the moveit_commander
+## module.  We also import rospy and some messages that we will use.
+import sys
+import copy
 import rospy
-from moveit_commander.robot_trajectory import RobotTrajectory
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+from visualization_msgs.msg import Marker
+# from ar_track_alvar_msgs.msg import AlvarMarkers
+## END_SUB_TUTORIAL
+
+from std_msgs.msg import String
+
+def main():
+    ## BEGIN_TUTORIAL
+    ##
+    ## Setup
+    ## ^^^^^
+    ## CALL_SUB_TUTORIAL imports
+    ##
+    ## First initialize moveit_commander and rospy.
+
+    rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
+    moveit_commander.roscpp_initialize(sys.argv)
+
+    group_name = "left_arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    robot = moveit_commander.RobotCommander()
+
+    ## Instantiate a PlanningSceneInterface object.  This object is an interface
+    ## to the world surrounding the robot.
+    scene = moveit_commander.PlanningSceneInterface()
+
+    ## Instantiate a MoveGroupCommander object.  This object is an interface
+    ## to one group of joints.  In this case the group is the joints in the left
+    ## arm.  This interface can be used to plan and execute motions on the left
+    ## arm.
+    scale = 1
+    waypoints = []
+
+    wpose = move_group.get_current_pose().pose
+    wpose.position.z -= scale + 0.01  # First move up (z)
+    wpose.position.y += scale + 0.02  # and sideways (y)
+    waypoints.append(copy.deepcopy(wpose))
+
+    wpose.position.x += scale + 0.01  # Second move forward/backwards in (x)
+    waypoints.append(copy.deepcopy(wpose))
+
+    wpose.position.y -= scale + 0.01  # Third move sideways (y)
+    waypoints.append(copy.deepcopy(wpose))
+
+    # We want the Cartesian path to be interpolated at a resolution of 1 cm
+    # which is why we will specify 0.01 as the eef_step in Cartesian
+    # translation.  We will disable the jump threshold by setting it to 0.0,
+    # ignoring the check for infeasible jumps in joint space, which is sufficient
+    # for this tutorial.
+    (plan, fraction) = move_group.compute_cartesian_path(
+        waypoints, 0.01  # waypoints to follow  # eef_step
+    )
+
+    move_group.execute(plan, wait=True)
+
+    # Calling `stop()` ensures that there is no residual movement
+    move_group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets().
+    move_group.clear_pose_targets()
+
+
+if __name__=='__main__':
+  try:
+    main()
+  except rospy.ROSInterruptException:
+    pass
